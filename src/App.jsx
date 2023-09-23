228845773e3f68ee87152ee4baaa4adf2e6be742
alexy piche <alexyautumn@gmail.com>
2023-09-23 19:08:27 -0400

    imported components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import './index.css'
 import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
+import Services from './components/experience/experience'
+import Portfolio from './components/portfolio/portfolio'
+import Testomonials from './components/testimonials/testimonials'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
 
 const App = () => {
   return (
     <>
        <Header />
+       <Nav />
+       <About />
+       <Experience />
+       <Services />
+       <Portfolio />
+       <Testomonials />
+       <Contact />
+       <Footer />
     </>
   )
 }
